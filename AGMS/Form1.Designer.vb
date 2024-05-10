2eca6ed1926266a9775c45fca30d12c4153f5bfe
vruttika2004 <vruttika2004@DESKTOP-46S3AJ4>
2024-05-10 17:18:53 +0530

    Add project files.

---
+++ AGMS/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Pink
+        Label1.BorderStyle = BorderStyle.Fixed3D
+        Label1.Font = New Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.Black
+        Label1.Location = New Point(196, 163)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(117, 30)
+        Label1.TabIndex = 0
+        Label1.Text = "Username :"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Pink
+        Label2.BorderStyle = BorderStyle.Fixed3D
+        Label2.Font = New Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.Black
+        Label2.Location = New Point(200, 255)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(110, 30)
+        Label2.TabIndex = 1
+        Label2.Text = "Password :"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(341, 157)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(244, 31)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(341, 252)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(244, 31)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Bisque
+        Button1.Font = New Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = Color.Black
+        Button1.Location = New Point(313, 351)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(144, 49)
+        Button1.TabIndex = 4
+        Button1.Text = "LOGIN"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.BurlyWood
+        Label3.BorderStyle = BorderStyle.FixedSingle
+        Label3.Font = New Font("Palatino Linotype", 16F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = Color.Black
+        Label3.Location = New Point(279, 40)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(228, 46)
+        Label3.TabIndex = 5
+        Label3.Text = "USER LOGIN"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(779, 450)
+        Controls.Add(Label3)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+
+End Class
---
+++ AGMS/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Dim connectionString As String = "Server='local';Database=Gallery;"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim username As String = TextBox1.Text
+        Dim password As String = TextBox2.Text
+
+        ' Connect to the database
+        Using connection As New SqlConnection(Data Source=(localdb)\ProjectModels;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False)
+            connection.Open()
+
+            ' Query to check if the username and password exist in the Login table
+            Dim queryString As String = "SELECT COUNT(*) FROM Login WHERE Username = @Username AND Password = @Password;"
+            Using command As New SqlCommand(queryString, connection)
+                command.Parameters.AddWithValue("@Username", username)
+                command.Parameters.AddWithValue("@Password", password)
+
+                ' Execute the query
+                Dim result As Integer = DirectCast(command.ExecuteScalar(), Integer)
+
+                If result > 0 Then
+                    ' Authentication successful
+                    MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    ' Authentication failed
+                    MessageBox.Show("Invalid username or password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            End Using
+        End Using
+    End Sub
+End Class
